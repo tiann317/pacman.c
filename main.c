1ed1cebd314ca9f3dd617acd74ccc6a7eb354c72
tiann317 <128642272+tiann317@users.noreply.github.com>
2024-04-08 19:11:54 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,43 +1,42 @@
-// +---------+
-// |    |    |
-// |    |    |
-// +----------+
-// |    |    |
-// |    |    |
-// +---------+ 
-//TODO: add random border to one distibuted quarter
-//TODO: add 2 dim arrays representation of the fields
+//TODO: score counter
+//TODO: fix eating dots after first 1-2 iterations
+//TODO: server/client
 #include <ncurses.h>
 #include <pthread.h>
 #include <unistd.h> 
 #include <stdlib.h> 
 #include <time.h>
-#define hight 30
+#define height 30
 #define width 40
-#define lhight 15
+#define lheight 15
 #define lwidth 20
 
 chtype pac_head = 'o';
 chtype wall = '#';
+chtype food = '.';
+chtype empt = ' ';
+
 int dir_x;
-char arr[hight][width];
-char larr[lhight][lwidth] = {
-    {'.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.'},
-    {'.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.'},
-    {'.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.'},
-    {'.','#','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.'},
-    {'.','#','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.'},
-    {'.','#','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.'},
-    {'.','#','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.'},
-    {'.','#','#','#','#','#','#','.','.','.','.','.','.','.','.','.','.','.','.','.'},
-    {'.','#','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.'},
-    {'.','#','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.'},
-    {'.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.'},
-    {'.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.'},
-    {'.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.'},
-    {'.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.'},
-    {'.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.'},
+char arr1[lheight][lwidth] = {
+    {'#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#'},
+    {'#','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.'},
+    {'#','.','.','.','.','.','.','.','.','.','.','.','.','#','.','.','.','.','.','.'},
+    {'#','.','.','.','.','.','.','.','.','.','.','.','.','#','.','.','.','.','.','.'},
+    {'#','.','.','.','#','.','.','.','.','.','.','.','.','#','.','.','.','.','.','.'},
+    {'#','.','.','.','#','.','.','.','.','.','.','.','.','#','.','.','.','.','.','.'},
+    {'#','.','.','.','#','.','.','.','.','.','.','.','.','#','.','.','#','#','#','#'},
+    {'#','.','.','.','#','#','#','.','.','.','.','.','.','#','.','.','#','.','.','.'},
+    {'#','.','.','.','#','.','.','.','.','.','.','.','.','#','.','.','#','.','.','.'},
+    {'#','.','.','.','#','.','.','.','.','.','.','#','#','#','.','.','#','.','.','.'},
+    {'#','.','.','.','#','.','.','.','#','#','#','#','.','.','.','.','#','.','.','.'},
+    {'#','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','#','.','.','.'},
+    {'#','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.'},
+    {'#','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.'},
+    {'#','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.'},
 };
+char arr2[lheight][lwidth];
+char arr3[lheight][lwidth];
+char arr4[lheight][lwidth];
 
 typedef struct point {
     int x;
@@ -49,39 +48,25 @@ typedef struct rect {
     struct point min;
 } Boundary;
 
-Point head;
+Point loghead;
+Point dsplhead;         //displayble head is the logical head but with offsets
 Boundary fullscr;
 Boundary boundary;
 Boundary quarter;
 
-static void fill(void) {
-    for (int i = boundary.min.x; i < boundary.max.x; i++) {
-        for (int j = boundary.min.y; j < boundary.max.y - 1; j++) {
-            arr[i][j] = '.';
-            mvaddch(i, j, arr[j][i]);
+static void logic_quarter(char arr[height][width]) {
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            if (arr[i][j] == '.') {
+                mvaddch(boundary.min.y + i, boundary.min.x + j, food);
+            } else if (arr[i][j] == ' ') {
+                mvaddch(boundary.min.y + i, boundary.min.x + j, empt);
+            } else {
+                mvaddch(boundary.min.y + i, boundary.min.x + j, wall);
+            }   
         }
-        mvaddch(0, i, ' ');
+        mvaddch(i, 0, empt);
     }
-}
-
-static void logic_quarter(void) {
-    for (int i = quarter.min.y; i < quarter.max.y; i++) {
-        for (int j = quarter.min.x; j < quarter.max.x - 1; j++)
-            mvaddch(i, j, larr[j][i]);  
-        mvaddch(0, i, ' ');
-    }
-}
-
-static void bound_print(void) {
-        for (int i = boundary.min.x - 2; i < boundary.max.x + 2; i++) {
-            mvaddch(boundary.min.y - 1, i, wall);
-            mvaddch(boundary.max.y , i, wall);
-        }
-        for (int j = boundary.min.y - 2; j < boundary.max.y + 2; j++)
-        {
-            mvaddch(j, boundary.min.x - 1, wall);
-            mvaddch(j, boundary.max.x, wall);
-        }
 }
 
 static void *input(void *none) {
@@ -92,19 +77,16 @@ static void *input(void *none) {
 }
 
 int main(void) {
-
     initscr();
     noecho();
     keypad(stdscr, TRUE);
-
     pthread_t pid;
     void *retval;
     pthread_create(&pid, NULL, input, NULL);
-
     bool done = FALSE;
-//    srand(time(NULL));
     
-    getmaxyx(stdscr, fullscr.max.y, fullscr.max.x);   
+    getmaxyx(stdscr, fullscr.max.y, fullscr.max.x); 
+
     boundary.max.x = fullscr.max.x/2 + 20;
     boundary.min.x = fullscr.max.x/2 - 20;
     boundary.max.y = fullscr.max.y/2 + 15;
@@ -114,50 +96,116 @@ int main(void) {
     quarter.min.y = boundary.min.y; 
     quarter.max.y = boundary.min.y + 15;
     quarter.max.x = boundary.min.x + 20;
-    
-    head.x = boundary.min.x + boundary.max.x/2;
-    head.y = boundary.min.y + boundary.max.y/2;
 
+    for (int i = 0; i < lheight; i++) {
+        for (int j = 0; j < lwidth; j++) 
+        arr2[i][j] = arr1[i][lwidth - 1 - j];        
+    }   
+
+    for (int i = 0; i < lheight; i++) {
+        for (int j = 0; j < lwidth; j++) 
+        arr3[i][j] = arr1[lheight - 1 - i][j];        
+    } 
+
+    for (int i = 0; i < lheight; i++) {
+        for (int j = 0; j < lwidth; j++) 
+        arr4[i][j] = arr1[lheight - 1 - i][lwidth - 1 - j];        
+    }     
+
+    char map[height][width];
+    for (int i = 0; i < height/2; i++) {
+        for (int j = 0; j < width/2; j++)
+        map[i][j] = arr1[i][j];
+    }
+    for (int i = 0; i < height/2; i++) {
+        for (int j = 0; j < width/2; j++)
+        map[height/2 + i][j] = arr3[i][j];
+    }
+    for (int i = 0; i < height/2; i++) {
+        for (int j = 0; j < width/2; j++)
+        map[i][width/2 + j] = arr2[i][j];
+    }
+    for (int i = 0; i < height/2; i++) {
+        for (int j = 0; j < width/2; j++)
+        map[height/2 + i][ width/2 + j] = arr4[i][j];
+    }
+
+    loghead.x = lwidth/2;
+    loghead.y = lheight/2;
+    dsplhead.x = loghead.x + quarter.min.x;
+    dsplhead.y = loghead.y + quarter.min.y;
+    
     while(TRUE) {
-        bound_print();
-//        fill();
-        logic_quarter();
-        if (mvprintw(1, 0, "tlhc x,y %d %d; brhc x,y %d %d", quarter.min.x, quarter.min.y,\
-        quarter.max.x, quarter.max.y) == ERR) {
+       logic_quarter(map);
+
+        if (mvprintw(0, 0, "bnd.min.x %d; mnd.min.y %d ", boundary.min.x, boundary.min.y) == ERR) {
             printw("Error! error %d", ERR);
         }
-        mvaddch(head.y, head.x, pac_head);
+        if (mvprintw(1, 0, "bnd.max.x %d; mnd.max.y %d ", boundary.max.x, boundary.max.y) == ERR) {
+            printw("Error! error %d", ERR);
+        }
+        mvaddch(dsplhead.y, dsplhead.x, pac_head);
         refresh();
-        mvaddch(head.y, head.x, ' ');
+        mvaddch(dsplhead.y, dsplhead.x, ' ');
         int tmp = dir_x; 
             switch (tmp) {
             case 's':
             case KEY_DOWN:
-                pac_head = 'V';
-                head.y = head.y >= boundary.max.y ? boundary.min.y : head.y; 
-                head.y++;           
+                pac_head = '^';
+                if (map[loghead.y + 1][loghead.x] == '.') {
+                    loghead.y++;
+                    dsplhead.y++;
+                    map[loghead.y][loghead.x] = ' ';
+                } else if (map[loghead.y + 1][loghead.x] == '#') {
+                    loghead.y = loghead.y;
+                } else {
+                    loghead.y++;
+                    dsplhead.y++;
+                }    
                 usleep(150000);
                 break;
             case 'w':
             case KEY_UP: 
-                pac_head = '^';
-                if (head.y <= boundary.min.y)
-                    head.y = boundary.max.y;
-                head.y--;
+                pac_head = 'v';
+                if (map[loghead.y - 1][loghead.x] == '.') {
+                    loghead.y--;
+                    dsplhead.y--;
+                    map[loghead.y][loghead.x] = ' ';
+                } else if (map[loghead.y - 1][loghead.x] == '#') {
+                    loghead.y = loghead.y;
+                } else {
+                    loghead.y--;
+                    dsplhead.y--;
+                }
                 usleep(150000);
                 break;
             case 'a':
             case KEY_LEFT:
-                pac_head = '<';
-                if (head.x <= boundary.min.x) 
-                    head.x = boundary.max.x;
-                head.x--;
+                pac_head = '>';
+                if (map[loghead.y][loghead.x - 1] == '.') {
+                    loghead.x--;
+                    dsplhead.x--;
+                    map[loghead.y][loghead.x] = ' ';
+                } else if (map[loghead.y][loghead.x - 1] == '#') {
+                    loghead.x = loghead.x;
+                } else {
+                    loghead.x--;
+                    dsplhead.x--;
+                }
                 break;
             case 'd':
             case KEY_RIGHT:
-                pac_head = '>';
-                head.x = head.x >= boundary.max.x ? boundary.min.x : head.x;
-                head.x++;
+                pac_head = '<';
+                if (map[loghead.y][loghead.x + 1] == '.') {
+                    loghead.x++;
+                    dsplhead.x++;
+                    map[loghead.y][loghead.x] = ' ';
+                } else if (map[loghead.y][loghead.x + 1] == '#') {
+                    loghead.x = loghead.x;
+                } else {
+                    loghead.x++;
+                    dsplhead.x++;
+                }
                 break;         
             case 'q':
                 done = TRUE;
